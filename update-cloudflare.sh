aa05f7cf942aedc520a827a44dde66bd5402700e
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2017-06-04 18:03:56 +0900

    Create update-cloudflare.sh

---
+++ update-cloudflare.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+while [[ $# -gt 1 ]] ; do
+  case $1 in
+    -k|--key)
+      auth_key=$2
+      shift
+      ;;
+    -e|--email)
+      auth_email=$2
+      shift
+      ;;
+    -z|--zone)
+      zone_name=$2
+      shift
+      ;;
+    -r|--record)
+      record_name=$2
+      shift
+      ;;
+    *)
+    ;;
+  esac
+  shift
+done
+
+[ -n "$auth_key" ] || { echo 'Missing auth key' ; exit 1 ; }
+[ -n "$auth_email" ] || { echo 'Missing auth email' ; exit 1 ; }
+[ -n "$zone_name" ] || { echo 'Missing zone name' ; exit 1 ; }
+[ -n "$record_name" ] || { echo 'Missing record name' ; exit 1 ; }
+
+ip=$(curl -s http://ipv4.icanhazip.com)
+ip_file="ip.txt"
+id_file="cloudflare.ids"
+
+# LOGGER
+log() {
+    if [ -n "$1" ]; then
+        echo -e "[$(date)] - $1"
+    fi
+}
+
+# SCRIPT START
+log "Check Initiated"
+
+if [ -f $ip_file ]; then
+    old_ip=$(cat $ip_file)
+    if [ $ip == $old_ip ]; then
+        echo "IP has not changed."
+        exit 0
+    fi
+fi
+
+if [ -f $id_file ] && [ $(wc -l $id_file | cut -d " " -f 1) == 2 ]; then
+    zone_identifier=$(head -1 $id_file)
+    record_identifier=$(tail -1 $id_file)
+else
+    zone_identifier=$(\
+      curl \
+        -s \
+        -X GET \
+        "https://api.cloudflare.com/client/v4/zones?name=$zone_name" \
+        -H "X-Auth-Email: $auth_email" \
+        -H "X-Auth-Key: $auth_key" \
+        -H "Content-Type: application/json" | \
+      grep -Po '(?<="id":")[^"]*' | head -1 )
+    record_identifier=$(\
+      curl \
+        -s \
+        -X GET \
+        "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name" \
+        -H "X-Auth-Email: $auth_email" \
+        -H "X-Auth-Key: $auth_key" \
+        -H "Content-Type: application/json" | \
+      grep -Po '(?<="id":")[^"]*')
+    echo "$zone_identifier" > $id_file
+    echo "$record_identifier" >> $id_file
+fi
+
+update=$(\
+  curl \
+    -s \
+    -X PUT \
+    "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" \
+    -H "X-Auth-Email: $auth_email" \
+    -H "X-Auth-Key: $auth_key" \
+    -H "Content-Type: application/json" \
+    --data "{\"id\":\"$zone_identifier\",\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\"}")
+
+if [[ $update == *"\"success\":false"* ]]; then
+    message="API UPDATE FAILED. DUMPING RESULTS:\n$update"
+    log "$message"
+    echo -e "$message"
+    exit 1 
+else
+    message="IP changed to: $ip"
+    echo "$ip" > $ip_file
+    log "$message"
+    echo "$message"
+fi
